108620025402cd0215d8278f4e8279aac2af3d40
Dileepa <wickramasinghemgdd@gmail.com>
2025-07-18 16:11:07 +0530

    Connected Mongo uri and filtered logs

--- foundation/src/main/java/com/example/foundation/FoundationApplication.java
+++ foundation/src/main/java/com/example/foundation/FoundationApplication.java
@@ -2,12 +2,17 @@ package com.example.foundation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class FoundationApplication {
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(FoundationApplication.class, args);
+        Environment env = context.getEnvironment();
 
-	public static void main(String[] args) {
-		SpringApplication.run(FoundationApplication.class, args);
-	}
-
+        String port = env.getProperty("server.port", "8080");
+        System.out.println("Application is running on http://localhost:" + port);
+    }
 }
