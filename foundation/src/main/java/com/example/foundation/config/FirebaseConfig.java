52deb2dbd92d1023270110429211f80807a5e8cb
Dileepa <wickramasinghemgdd@gmail.com>
2025-07-19 09:27:58 +0530

    Configured Firebase files

---
+++ foundation/src/main/java/com/example/foundation/config/FirebaseConfig.java
@@ -0,0 +1,34 @@
+package com.example.foundation.config;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.springframework.context.annotation.Configuration;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+import jakarta.annotation.PostConstruct;
+
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void initFirebase() {
+        try {
+            FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase-key.json");
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to initialize Firebase", e);
+        }
+    }
+}
+
