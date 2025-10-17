9c16a642c7d8e0f3d86e04bcb1186df842f2243a
Dileepa <wickramasinghemgdd@gmail.com>
2025-10-17 23:26:16 +0530

    deleted frontend

--- frontend/src/config/axiosIntance.tsx
+++
@@ -1,8 +0,0 @@
-// src/services/axios.ts
-import axios from "axios";
-
-const backend_Path = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL, // .env file
-});
-
-export default backend_Path;
